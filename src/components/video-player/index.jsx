ad472b8d1d05b21034505098e265c885d6db8ea9
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2022-11-17 20:22:37 -0300

    feat: Adding video player

--- src/components/video-player/index.jsx
+++ src/components/video-player/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import ReactPlayer from "react-player/file";
+import "node_modules/video-react/dist/video-react.css";
+import { Player } from "video-react";
 
-function VideoPlayer({ url }) {
-    console.log({url})
+function VideoPlayer({ url, thumbnail }) {
+    console.log({ url });
     return (
-        <video width="919" height={517} controls>
-            <source src={url} type="video/mp4" />
-        </video>
+        // <video width="919" height={517} controls>
+        //     <source src={url} type="video/mp4" />
+        // </video>
+        <Player
+            playsInline
+            poster={thumbnail}
+            src={url}
+            fluid={false}
+            width={919}
+            height={517}
+        />
     );
 }
 
